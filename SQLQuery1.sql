cd2c343ebd9466b7ae81df3ad1bb85b9986893ba
Davi <davilucastellesbarbosa@gmail.com>
2026-06-16 20:25:37 -0300

    SERVICES PROFESSOR

---
+++ SQLQuery1.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE salas (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    andar INT NOT NULL,
+    quantidadeassentos INT NOT NULL
+);
+
+CREATE TABLE reservas (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    inicio DATETIME NOT NULL,
+    fim DATETIME NOT NULL,
+    salaid INT NOT NULL,
+
+    CONSTRAINT FK_Reserva_Sala
+        FOREIGN KEY (salaid)
+        REFERENCES salas(id)
+);
